ca27e22b4b2457d71e58db0b5e3377ef014a8bd2
Shreejit K <JC87@VDI01T11HSC-518.dotcom.tesco.org>
2016-09-26 14:50:42 +0100

    [Shreejit]- Green - Testing arguments length

---
+++ src/main/java/com/kataskills/App.java
@@ -0,0 +1,12 @@
+package com.kataskills;
+
+/**
+ * Created by JC87 on 9/26/2016.
+ */
+public class App {
+
+    public static void main(String[] argList) throws Exception {
+        if (argList.length != 1)
+            throw new Exception("One input parameter is expected");
+    }
+}
