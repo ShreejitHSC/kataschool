23a52ebc98cc6b976f705cf0bb1bd414bdef5bf9
Shreejit K <JC87@VDI01T11HSC-518.dotcom.tesco.org>
2016-09-26 12:58:07 +0100

    [Shreejit]- Driver class created to take input

---
+++ src/main/java/com/kataskills/App.java
@@ -0,0 +1,25 @@
+package com.kataskills;
+
+import java.util.List;
+import static java.util.Arrays.asList;
+
+/**
+ * Created by JC87 on 9/26/2016.
+ */
+public class App {
+
+
+    public static void main(String[] argList) throws Exception {
+        List<String> validInputValues = asList("rock", "paper", "scissors");
+
+        if (argList.length != 1)
+            throw new Exception("One input parameter is expected");
+
+        if (validInputValues.contains(argList[0].toLowerCase())) {
+            //Driver class to call the method
+        } else {
+            throw new Exception("The input provided is not a valid one");
+        }
+
+    }
+}
