60bc5dd3280e8f98ecbeab3cc5f5def3c9a83b6a
Shreejit K <shreejit.kakkassery@in.tesco.com>
2016-10-03 17:43:11 +0530

    [Shreejit] - GREEN - Fixed the first test case

---
+++ src/main/java/com/kataskills/KarateChop.java
@@ -0,0 +1,19 @@
+package com.kataskills;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Created by skakk2 on 10/3/2016.
+ */
+public class KarateChop {
+
+
+    public List sortArray(List unsortedIntArray) {
+        Collections.sort(unsortedIntArray);
+        return unsortedIntArray;
+    }
+}
