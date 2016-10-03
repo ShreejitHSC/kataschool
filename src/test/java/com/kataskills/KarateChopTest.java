77868d08bd74597c41c950190af48baff1402527
Shreejit K <shreejit.kakkassery@in.tesco.com>
2016-10-03 17:49:04 +0530

    [Shreejit] - REFACTOR - Changed the class intialization to Before() in test

--- src/test/java/com/kataskills/KarateChopTest.java
+++ src/test/java/com/kataskills/KarateChopTest.java
@@ -1,5 +1,6 @@
 package com.kataskills;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,12 +13,17 @@ import static org.junit.Assert.*;
  * Created by skakk2 on 10/3/2016.
  */
 public class KarateChopTest {
+    private KarateChop karateChop;
+
+    @Before
+    public void before(){
+        karateChop = new KarateChop();
+    }
 
     @Test
     public void validateTheArrayPassedIsSorted(){
-        List unsortedArrayList = new ArrayList<Integer>(asList(1,3,5,7,2,4,6,8));
-        List expectedSortedIntArray = new ArrayList<Integer>(asList(1,2,3,4,5,6,7,8));
-        assertEquals(expectedSortedIntArray,new KarateChop().sortArray(unsortedArrayList));
-
+        List unsortedArrayList = new ArrayList<>(asList(1, 3, 5, 7, 2, 4, 6, 8));
+        List expectedSortedIntArray = new ArrayList<>(asList(1, 2, 3, 4, 5, 6, 7, 8));
+        assertEquals(expectedSortedIntArray,karateChop.sortArray(unsortedArrayList));
     }
 }
